a43c4143f3b97aaa7607eb9a9da3785885ec110c
Guilherme <ludewon@gmail.com>
2024-08-14 01:19:59 -0300

    1.0 version

---
+++ php/requestOfAPI.php
@@ -0,0 +1,57 @@
+<?php
+	$indexRequestCep = $_POST['request_cep'] ?? '01153-000';
+
+	$apiKeys = array(
+			'apiKeyCep'=>'YourApiKey',
+			'apiKeyClima'=>'YourApiKey'
+		);
+
+	$urlResquestApiCep = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$indexRequestCep.'&key='.$apiKeys['apiKeyCep'];
+	
+	function getResponseApi($url){
+			$curl = curl_init();
+			curl_setopt($curl,CURLOPT_URL,$url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			$responseApi = json_decode(curl_exec($curl),true);
+			curl_close($curl);
+			return $responseApi;
+		}
+
+	$isSetCep = isset($indexRequestCep);
+	
+	if($isSetCep){
+
+		$responseApiCep = getResponseApi($urlResquestApiCep);
+
+		$responseApiCepLatLong = array(
+			'latitude'=>$responseApiCep['results']['0']['geometry']['location']['lat'],
+			'longitude'=>$responseApiCep['results']['0']['geometry']['location']['lng']
+		);
+
+		$isSetResponseApiCepLatLong = isset($responseApiCepLatLong['latitude'],$responseApiCepLatLong['longitude']);
+
+		if($isSetResponseApiCepLatLong){
+			$urlRequestApiClima = 'https://api.openweathermap.org/data/2.5/weather?lat='.$responseApiCepLatLong['latitude'].'&lon='.$responseApiCepLatLong['longitude'].'&appid='.$apiKeys['apiKeyClima'];
+
+			$responseApiClima = getResponseApi($urlRequestApiClima);
+
+			$responseApiClimaDados = array(
+				'cidadeSelecionada'=>$responseApiClima['name'],
+				'climaAtual'=>$responseApiClima['weather']['0']['main'],
+				'descricaoClimaAtual'=>$responseApiClima['weather']['0']['description'],
+				'velocidadeVentoAtual'=>$responseApiClima['wind']['speed'],
+				'temperaturaAtual'=>$responseApiClima['main']['temp'] - 273.15,
+				'temperaturaMinimaAtingida'=>$responseApiClima['main']['temp_min'] - 273.15 ,
+				'temperaturaMaximaAtingida'=>$responseApiClima['main']['temp_max'] - 273.15,
+				'umidadeAtual'=>$responseApiClima['main']['humidity']);
+
+			return $responseApiClimaDados;
+			
+
+		}else{
+			echo '<h1 style="color:red">Erro : Sem Latitude ou Longitude !</h1>';
+		}
+	}
+
+?>
+
